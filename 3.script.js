9af37d2d973bebcb389c43247e6a98a66108acb4
abinavan <126337534+Abinavan19@users.noreply.github.com>
2023-06-20 11:22:42 +0530

    day 6 task

---
+++ 3.script.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName, lastName, age, gender, maritalStatus, phoneNo, emailId){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+      this.maritalStatus = maritalStatus;
+      this.phoneNo = phoneNo;
+      this.emailId = emailId;
+    }
+  }
+  
+  let ram = new Person("Abi", "Navan", 22, male , "Single",7339036778, "abinavanraja@gmail.com")
